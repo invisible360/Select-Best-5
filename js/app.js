7073d527c637a4de82d32c627292a942a12db50b
invisible360 <zakirh817@gmail.com>
2022-08-22 05:13:06 +0600

    selected v done without validation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log ('apps connected')
+document.getElementById('all-players-container').addEventListener('click', function (event) {
+    const selectBtnElement = event.target;
+    const selectBtnText = event.target.innerText;
+    const playerName = selectBtnElement.parentNode.firstChild.nextSibling.innerText;
+    const playerNameLowerCase = playerName.toLowerCase();
+
+    if ((selectBtnText === 'Select') && (playerNameLowerCase.includes('messi'))) {
+        playerListCheck('messi');
+    }
+    
+    else if ((selectBtnText === 'Select') && (playerNameLowerCase.includes('neymar'))) {
+        playerListCheck('neymar');
+    }
+    
+    else if ((selectBtnText === 'Select') && (playerNameLowerCase.includes('mbappe'))) {
+        playerListCheck('mbappe');
+    }
+    
+    else if ((selectBtnText === 'Select') && (playerNameLowerCase.includes('Vitor'))) {
+        playerListCheck('Vitor');
+    }
+    
+    else if ((selectBtnText === 'Select') && (playerNameLowerCase.includes('ramos'))) {
+        playerListCheck('ramos');
+    }
+    else {
+        playerListCheck('renato');
+
+    }
+
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,23 @@
-console.log ('utilities connected')
+function playerListCheck(idName) {
+    const playersList = document.querySelectorAll('li')
+    for (let i = 1; i <= playersList.length; i++) {
+        if (playersList[i - 1].innerText.includes('Player-' + i + '')) {
+            const getId = playersList[i - 1].getAttribute('id');
+            const getName = getText(idName);
+            setText(getId, getName);
+            document.getElementById(idName + '-' + 'btn').setAttribute('disabled', true);
+            break;
+        }
+    }
+}
+
+function getText (id) {
+    const getTextElement = document.getElementById (id);
+    const getText = getTextElement.innerText;
+    return getText;
+}
+
+function setText (id, value) {
+    const setTextElement = document.getElementById (id);
+    setTextElement.innerText = value;
+}
