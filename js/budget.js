1e154c80e67d3d67fc95365f77a185f1c9936bdb
invisible360 <zakirh817@gmail.com>
2022-08-22 16:49:12 +0600

    budget done without validation

--- js/budget.js
+++ js/budget.js
@@ -1,1 +1,25 @@
 // Budget Calculation
+document.getElementById ('total-cost-of-each-players-btn').addEventListener('click', function () {
+    const playersList = document.querySelectorAll('li');
+    const myArray = coverArrayLikeObjectToArray (playersList);
+    
+    let count = 0;
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i].includes('player') === false) {
+            count++;
+        }
+    }
+
+    //Only Selected Player cost Calculation
+    const perPlayerCost = getInputValueAsNumber ('per-player-cost');
+    const totalCostOfSelectedPlayers = perPlayerCost * count;
+    document.getElementById('players-expenses-amount').innerText = totalCostOfSelectedPlayers;
+})
+
+document.getElementById ('total-cost-btn').addEventListener ('click', function () {
+    const playerExpenses = getTextValueAsNumber ('players-expenses-amount');
+    const managerCost = getInputValueAsNumber ('manager-cost');
+    const coachCost = getInputValueAsNumber ('coach-cost');
+    const totalCost = playerExpenses + managerCost + coachCost;
+    document.getElementById ('total-amount').innerText = totalCost;
+})
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,5 @@
 function playerListCheck(idName) {
-    const playersList = document.querySelectorAll('li')
+    const playersList = document.querySelectorAll('li');
 
     //placing name in selected-v list
     for (let i = 1; i <= playersList.length; i++) {
@@ -8,26 +8,24 @@ function playerListCheck(idName) {
             const getName = getText(idName);
             setText(getId, getName);
             document.getElementById(idName + '-' + 'btn').setAttribute('disabled', true);
+            document.getElementById ('players-expenses-amount').innerText = '0000';
+            document.getElementById ('total-amount').innerText = '0000';
+            document.getElementById ('per-player-cost').value = '';
+            document.getElementById ('manager-cost').value = '';
+            document.getElementById ('coach-cost').value = '';
             break;
         }
     }
 
     //validation for more than 5 players selection
-
-    // convert array like object to original array 
-    const updatedPlayerListArray = [];
-    for (let i = 0; i < playersList.length; i++) {
-        updatedPlayerListArray.push(playersList[i].innerText.toLowerCase());
-    }
-
+    const myArray = coverArrayLikeObjectToArray (playersList)
+    
     let count = 0;
-    for (let i = 0; i < updatedPlayerListArray.length; i++) {
-        if (updatedPlayerListArray[i].includes('player') === false) {
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i].includes('player') === false) {
             count++;
-            console.log(count);
             if (count === 5) {
                 count2++;
-                console.log(count2);
                 if (count2 > 1) {
                     alert('5 Players Already Selected');
                 }
@@ -35,6 +33,16 @@ function playerListCheck(idName) {
         }
     }
 }
+
+// convert array like object to original array 
+function coverArrayLikeObjectToArray (arrayLikeObject) {
+    const updatedPlayerListArray = [];
+    for (let i = 0; i < arrayLikeObject.length; i++) {
+        updatedPlayerListArray.push(arrayLikeObject[i].innerText.toLowerCase());
+    }
+    return updatedPlayerListArray;
+}
+
 
 function getText (id) {
     const getTextElement = document.getElementById (id);
@@ -47,4 +55,19 @@ function setText (id, value) {
     setTextElement.classList.remove('text-secondary');
     setTextElement.classList.add('text-white');
     setTextElement.innerText = value;
+}
+
+function getInputValueAsNumber (id) {
+    const inputElement = document.getElementById (id);
+    const inputVlueString = inputElement.value;
+    const inputValue = parseFloat (inputVlueString);
+    // inputElement.value ='';
+    return inputValue;
+}
+
+function getTextValueAsNumber (id) {
+    const textField = document.getElementById (id);
+    const textAsString = textField.innerText;
+    const textNumber = parseFloat (textAsString);
+    return textNumber;
 }
